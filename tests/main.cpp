92ac8a19a475426b3b5e3c5f9e8a4ebeee842411
Eugene <lin.eugene.l.e@gmail.com>
2023-06-13 22:02:44 +0800

    Initialize the time zone for testing

--- tests/main.cpp
+++ tests/main.cpp
@@ -2,6 +2,11 @@
 
 int main()
 {
+    // initialize the time zone for testing
+    char env[20];
+    strcpy(env, "TZ=UTC-8");
+    putenv(env);
+    tzset();
     testing::InitGoogleTest();
     return RUN_ALL_TESTS();
 }
